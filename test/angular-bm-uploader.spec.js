f3f7a89eae984e3a4d11730c7176ff05d18fdb9d
Kieren Powell <kieren@blinkmobile.com.au>
2017-08-31 14:32:12 +1000

    FSS-12 # Add controller test

--- test/angular-bm-uploader.spec.js
+++ test/angular-bm-uploader.spec.js
@@ -1,9 +1,38 @@
 'use strict'
+/* eslint-disable no-undef */
 
-describe('bmBlobUploader', () => {
-  beforeEach(angular.mock.module('bmBlobUploader'))
+describe('bm image controller', () => {
+  let $componentController
+  let locals
+  let $ctrl
+  beforeEach(module('bmUploader'))
 
-  it('should do something', (done) => {
+  beforeEach(inject(function (_$componentController_, _$rootScope_) {
+    $componentController = _$componentController_
+    locals = {
+      $scope: _$rootScope_.$new(),
+      bmUploaderService: {
+        retrieveContentUrl: () => Promise.resolve('123')
+      }
+    }
+    $ctrl = $componentController('bmImage', locals, {imageId: '123'})
+  }))
+
+  it('should call service when imageId changes', (done) => {
+    const $onSpy = spyOn(locals.bmUploaderService, 'retrieveContentUrl').and.callThrough()
+    $ctrl.$onChanges({
+      imageId: {
+        currentValue: '123'
+      }
+    })
+    expect($onSpy).toHaveBeenCalled()
+    done()
+  })
+
+  it('should NOT call service when imageId does not change', (done) => {
+    const $onSpy = spyOn(locals.bmUploaderService, 'retrieveContentUrl').and.callThrough()
+    $ctrl.$onChanges({})
+    expect($onSpy).not.toHaveBeenCalled()
     done()
   })
 })
